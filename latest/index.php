849cd2bc71d9936381b0a7340f876784cc496e7c
Fabrizio Salmi <fabrizio.salmi@gmail.com>
2013-05-25 13:38:58 +0200

    major fixes
    prettify removed
    css files minified and updated
    other little fixes
    improved Google PageSpeed score: 95/100

--- latest/index.php
+++ latest/index.php
@@ -9,26 +9,13 @@
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <!-- Styles -->
-        <link rel="stylesheet" type="text/css" media="only screen and (max-device-width: 480px)" href="http://millionmediapages.com/assets/css/bootstrap.css">
-        <link rel="stylesheet" type="text/css" media="only screen and (min-device-width: 768px)" href="http://millionmediapages.com/assets/css/bootstrap.css">
+        <link rel="stylesheet" type="text/css" media="only screen and (max-device-width: 480px)" href="http://millionmediapages.com/assets/css/bootstrap.min.css">
+        <link rel="stylesheet" type="text/css" media="only screen and (min-device-width: 768px)" href="http://millionmediapages.com/assets/css/bootstrap.min.css">
         <link rel="stylesheet" type="text/css" media="print, handheld" href="http://millionmediapages.com/assets/css/bootstrap.css">
         <link href="http://millionmediapages.com/assets/css/bootstrap-responsive.css" rel="stylesheet" type="text/css">
-        <link href="http://millionmediapages.com/assets/css/bootstrap.css" rel="stylesheet" type="text/css">
+        <link href="http://millionmediapages.com/assets/css/bootstrap.min.css" rel="stylesheet" type="text/css">
         <link href="http://millionmediapages.com/assets/css/style.css" rel="stylesheet" type="text/css">
-        <link href="http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,400,600,700,300|Bree+Serif" rel="stylesheet" type="text/css">
-        <link href="http://millionmediapages.com/assets/js/google-code-prettify/prettify.css" rel="stylesheet" type="text/css">
         <link rel="alternate" type="application/rss+xml" title="RSS" href="http://millionmediapages.com/feed.xml">
-        <!--[if IE]>
-             <link href="http://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet" type="text/css">
-             <link href="http://fonts.googleapis.com/css?family=Open+Sans:300italic" rel="stylesheet" type="text/css">
-             <link href="http://fonts.googleapis.com/css?family=Open+Sans:400italic" rel="stylesheet" type="text/css">
-             <link href="http://fonts.googleapis.com/css?family=Open+Sans:600italic" rel="stylesheet" type="text/css">
-             <link href="http://fonts.googleapis.com/css?family=Open+Sans:700italic" rel="stylesheet" type="text/css">
-             <link href="http://fonts.googleapis.com/css?family=Open+Sans:300" rel="stylesheet" type="text/css">
-             <link href="http://fonts.googleapis.com/css?family=Open+Sans:400" rel="stylesheet" type="text/css">
-             <link href="http://fonts.googleapis.com/css?family=Open+Sans:600" rel="stylesheet" type="text/css">
-             <link href="http://fonts.googleapis.com/css?family=Bree+Serif:400" rel="stylesheet" type="text/css">
-             <![endif]-->
         <!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
         <!--[if lt IE 9]>
               <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
@@ -40,7 +27,7 @@
     <body>
         <div id="header" class="masthead">
             <div class="container">  	
-                <h1 class="font-bree-serif">millionmediapages</h1>
+                <h1 class="font-bree-serif">Million Media Pages</h1>
                 <a class="accessible" href="#content">[Skip to Content]</a>
 
                 <div class="navbar">
@@ -51,7 +38,6 @@
                             <li><a href="http://millionmediapages.com/privacy/">Privacy</a></li> 
                         </ul>
                         <form class="navbar-search pull-right">
-                            <a href="http://millionmediapages.com/sitemap.xml">Sitemap</a> | <a href="http://millionmediapages.com/feed.xml">RSS</a> | <a href="https://www.facebook.com/MillionMediaPages">Facebook</a> | <a href="https://plus.google.com/u/0/105226994279545398535/">Google+</a>
 
                             <input title="search" type="text" class="search-query" />
                         </form>
@@ -97,6 +83,7 @@
             <div class="container">
                 <div class="row">
 
+                            <a href="http://millionmediapages.com/sitemap.xml">Sitemap</a> | <a href="http://millionmediapages.com/feed.xml">RSS</a> | <a href="https://www.facebook.com/MillionMediaPages">Facebook</a> | <a href="https://plus.google.com/u/0/105226994279545398535/">Google+</a>
 
                 </div><!--end row-->  
             </div><!--end container-->  
@@ -104,43 +91,8 @@
         <!-- Placed at the end of the document so the pages load faster -->
         <script src="http://millionmediapages.com/assets/js/jquery.js"></script>
         <script src="http://millionmediapages.com/assets/js/bootstrap.min.js"></script>
-        <script src="http://millionmediapages.com/assets/js/google-code-prettify/prettify.js"></script>
         <script type="text/javascript" src="http://millionmediapages.com/assets/js/respond.js"></script>
-        <script type="text/javascript">
-            $(function(){
-                //Load Goodle Code Pretiffy		   
-                window.prettyPrint && prettyPrint();
-                //Tab function
-                $('#pluginTab a').click(function (e) {
-                    e.preventDefault();
-                    $(this).tab('show');
-                });
-                //FAQ autosuggest
-                $('.typeahead').typeahead();
-                //tooltip init
-                $("a.tip").tooltip();	
-                //footer link to got top
-                gotoTop();
-                function gotoTop(){
-                    // scroll body to 0px on click
-                    $('a#top').on('click',function () {
-                        $('body,html').animate({
-                            scrollTop: 0
-                        }, 800);
-                        return false;
-                    });
-                };
-            });
-        </script>
-        <script>
-            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-                    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-                m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-            })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-            ga('create', 'UA-41184708-1', 'millionmediapages.com');
-            ga('send', 'pageview');
-
-        </script>
+       <!-- Google Analytics -->
+        <script>(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','//www.google-analytics.com/analytics.js','ga');ga('create','UA-41184708-1','millionmediapages.com');ga('send','pageview');</script>
     </body>
 </html>
